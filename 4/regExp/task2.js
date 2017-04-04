3457ea68668f0bb765bfbf7e84be3925ed7a2989
Alexandra Kreidich <kreidichalexandra@gmail.com>
2017-04-04 18:27:42 +0300

    regExp invalid data checker added

--- 4/regExp/task2.js
+++ 4/regExp/task2.js
@@ -1,5 +1,6 @@
 inputButton.onclick = function() {
     var str = document.getElementById('inputText').value;
+    console.log(str);
     if (str != "") {
         var t = /(\+?\d{1,3})[(]?(\d{1,2})[)]?[-\s]?(\d{3})[-\s]?(\d{2})[-\s]?(\d{2})/;
         if (t.exec(str)) {
@@ -11,10 +12,10 @@ inputButton.onclick = function() {
             var p = document.createElement('p');
             p.innerHTML = 'номер телефона: ' + res;
             container.appendChild(p);
+        } else {
+            var p = document.createElement('p');
+            p.innerHTML = 'вы не ввели данные';
+            container.appendChild(p);
         }
-    } else {
-        var p = document.createElement('p');
-        p.innerHTML = 'вы не ввели данные';
-        container.appendChild(p);
     }
 }
