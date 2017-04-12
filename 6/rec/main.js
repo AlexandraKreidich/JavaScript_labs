b4a1ee3e2b20f2a86e74786928b4afbb2c71744d
Alexandra Kreidich <kreidichalexandra@gmail.com>
2017-04-12 09:10:54 +0300

    rec changed

--- 6/rec/main.js
+++ 6/rec/main.js
@@ -1,15 +1,16 @@
 var elements = document.documentElement.childNodes;
 var textElem = 0; //количество тектовых
 var commentElem = 0; //количество коментариев
+var tags = 0;
 
 function rec(elements) {
     [].forEach.call(elements, function(elem) {
         if (elem.nodeName == '#text') {
             textElem++;
-        }
-        else if (elem.nodeName == '#comment') {
+        } else if (elem.nodeName == '#comment') {
             commentElem++;
         } else {
+            tags++;
             console.log(elem); //elem.nodeName
             return rec(elem.childNodes);
         }
@@ -19,3 +20,4 @@ rec(elements);
 console.log(' ');
 console.log('количество текстовых элементов: ' + textElem);
 console.log('количество комментариев: ' + commentElem);
+console.log('количество элементов: ' + tags);
