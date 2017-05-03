0d3a45c54ff0a8587367931910ad2547a3d7977f
Alexandra Kreidich <kreidichalexandra@gmail.com>
2017-05-03 15:40:07 +0300

    task 10 finished

--- 10/js/script.js
+++ 10/js/script.js
@@ -8,6 +8,10 @@ function Product(name, type, price, dateOfProd) {
     this.dateOfProd = new Date(dateOfProd);
 };
 
+Product.prototype.toString = function() {
+    return '<p>Name: ' + this.name + '</p><p>Type: ' + this.type + '</p><p>Price: ' + this.price + '</p><p>Prodused: ' + this.dateOfProd.getDate() + ' ' + this.dateOfProd.getDay() + ' ' + this.dateOfProd.getFullYear() + '</p>';
+}
+
 function FoodProduct(name, type, price, dateOfProd, exp, store) {
     Product.apply(this, [name, type, price, dateOfProd]);
     this.exp = new Date(exp);
@@ -21,6 +25,11 @@ function FoodProduct(name, type, price, dateOfProd, exp, store) {
 
 FoodProduct.prototype = new Product();
 FoodProduct.prototype.constructor = FoodProduct;
+FoodProduct.prototype.toString = function() {
+    return '<p>Name: ' + this.name + '</p><p>Type: ' + this.type + '</p><p>Price: ' + this.price + '</p><p>Prodused: ' +
+        this.dateOfProd.getDate() + ' ' + this.dateOfProd.getDay() + ' ' + this.dateOfProd.getFullYear() + '</p><p>Exp: ' +
+        this.exp.getDate() + ' ' + this.exp.getDay() + ' ' + this.exp.getFullYear() + '</p><p>Store: ' + this.store + '</p>';
+}
 
 function Shop(name, adress, markup, income) {
     this.name = name;
@@ -71,10 +80,7 @@ function Shop(name, adress, markup, income) {
     Object.defineProperty(this, 'toString', {
         get: function() {
             var str = '';
-            this.products.forEach(function(elem) {
-                str += 'продукт ' + elem.product.name + ' в количестве: ' + elem.ammount;
-            });
-            return "[магазин " + this.name + " находится по адресу " + this.adress + ". Список продуктов: " + str + ". Наценка магазина: " + this.markup + ", доход: " + this.income + ']';
+            return "<p>магазин " + this.name + "</p><p> находится по адресу " + this.adress + ".</p><p>Наценка магазина: " + this.markup + "</p><p> доход: " + this.income + '</p>';
         },
         set: function(e) {
             console.log("you can't change this property into " + e);
@@ -109,7 +115,7 @@ goodsForShopBelarus.forEach(function(elem) {
 
 /*----------------------SHOP WAKEUP----------------------------*/
 var Wakeup = new Shop("Wakeup", "st.Vrublevskogo h.190", 10, 0);
-var goodsForShopWakeup = new Array(new FoodProduct("ice-cream", "milk product", 2, "Apr 4, 2017", "Aug 4, 2017", 90), new FoodProduct("milk", "milk product", 2, "May 5, 2017", "May 25, 2017", 20), new FoodProduct("potato", "vegetables", 1.5, "May 5, 2017", "Nov 5, 2017", 180),
+var goodsForShopWakeup = new Array(new FoodProduct("ice-cream", "milk product", 5, "Apr 4, 2017", "Aug 4, 2017", 90), new FoodProduct("milk", "milk product", 2, "May 5, 2017", "May 25, 2017", 20), new FoodProduct("potato", "vegetables", 1.5, "May 5, 2017", "Nov 5, 2017", 180),
     new FoodProduct("cucumbers", "vegetables", 5, "May 5, 2017", "Jun 5, 2017", 30), new FoodProduct("spinach", "vegetables", 0.5, "May 5, 2017", "May 11, 2017", 6),
     new FoodProduct("peach", "fruits", 4.4, "May 5, 2017", "May 20, 2017", 15), new FoodProduct("strawberry", "fruits", 10, "May 5, 2017", "May 19, 2017", 14),
     new FoodProduct("bread", "bread", 1, "May 5, 2017", "May 12, 2017", 7), new FoodProduct("donuts", "bread", 3, "May 5, 2017", "May 12, 2017", 7),
@@ -131,9 +137,14 @@ goodsForShopAlbus.forEach(function(elem) {
     Albus.addProduct(elem, Math.floor(Math.random() * 100));
 });
 
+var shop = '';
 var onShop = function(e) {
-    if (this != e.target)
+    var info = document.getElementById('info');
+    info.innerHTML = '';
+    if (this != e.target) {
         Market.showProductsForShop(e.target.value);
+        shop = e.target.value;
+    }
 };
 
 var onProduct = function(e) {
@@ -141,6 +152,7 @@ var onProduct = function(e) {
     if (this != e.target) {
         var result = e.target.textContent.match(regexp);
         var prodName = result[1];
+        Market.showInfoAboutProduct(shop, prodName);
     }
 }
 
@@ -161,22 +173,70 @@ var Market = {
     },
     showProductsForShop: function(nameOfShop) {
         var div = document.getElementById('goods');
+        var div2 = document.getElementById('statistics');
+        div2.innerHTML = '';
         var str = '<div class="list-group">'
         this.shops.forEach(function(elem) {
             if (nameOfShop == elem.name) {
                 elem.products.forEach(function(elem) {
                     str += '<a class="list-group-item"><span class="badge">' + elem.ammount + '</span> ' + elem.product.name + '</a>';
                 });
+                div2.innerHTML = elem.toString;
             };
         });
         str += '</div>';
         div.innerHTML = str;
         div.addEventListener('click', onProduct);
     },
-    showInfoAboutProduct: function (nameOfProd) {
-      var div = document.getElementById('info');
-      var str = '<div class="input-group">'
-      this.
+    showInfoAboutProduct: function(shop, prodName) {
+        var div = document.getElementById('info');
+        var str = '<div class="container"><p>Descripion of product:</p>';
+        this.shops.forEach(function(elem) {
+            if (elem.name == shop) {
+                elem.products.forEach(function(el) {
+                    if (el.product.name == prodName) {
+                        str += el.product.toString();
+                        str += '<p>Ammount: ' + el.ammount + '</p>';
+                        str += '<input type="number" class="amount" value="1" min="1" max="' + el.ammount + '" ><input type="button" class="b" value="cell" data-id="' + el.product.id + '"><input type="button" class="b" value="delete" data-id="' + el.product.id + '">'
+                    };
+                });
+            };
+        });
+        str += '</div>';
+        div.innerHTML = str;
+        var btns = document.getElementsByClassName('b');
+        [].forEach.call(btns, function(elem) {
+            elem.addEventListener('click', function(e) {
+                amount = document.getElementsByClassName('amount');
+                amount = amount[0].value;
+                prodID = this.getAttribute('data-id');
+                Market.shops.forEach(function(elem) {
+                    if (elem.name == shop) {
+                        elem.products.forEach(function(el) {
+                            if (el.product.id == Number(prodID)) {
+                                if (e.target.value == 'cell') {
+                                    if (amount <= el.ammount) {
+                                        elem.cellProduct(el.product.name, amount);
+                                    } else {
+                                        console.log('error');
+                                    }
+                                } else {
+                                    if (amount <= el.ammount) {
+                                        elem.deleteProduct(el.product.name, amount);
+                                    } else {
+                                        console.log('error');
+                                    }
+                                }
+                            }
+                        })
+                        var div = document.getElementById('statistics');
+                        div.innerHTML = elem.toString;
+                    }
+                })
+
+
+            });
+        });
     }
 };
 
